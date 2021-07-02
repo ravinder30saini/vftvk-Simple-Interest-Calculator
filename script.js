5ffcff524c158246f30c28a6124cd5ae83335ae1
Ravinder Kaur <ravinder30saini@gmail.com>
2021-07-02 05:55:48 +0000

    The application changes

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    if(principal <= 0 || principal == ""){
+        alert("Enter a positive value");
+        document.getElementById("principal").focus();
+    }
+    else {
+        var interest = principal*years*rate/100;
+        var amount = principal+interest;
+        var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+",\<\\mark\>\<br\>at an interest rate of \<mark\>"+rate+"%\<\\mark\>\<br\>You will receive an amount of \<mark\>"+amount+",\<\\mark\>\<br\>in the year \<mark\>"+year+"\<\\mark\>\<br\>"
+
+    }
 }
-        
+   
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
